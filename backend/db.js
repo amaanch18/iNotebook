616862d0b79d27ddd892c7ac1f72a44e207a910a
amaanch18 <amaanch18@gmail.com>
2022-10-07 23:44:51 +0530

    oct 7

---
+++ backend/db.js
@@ -0,0 +1,10 @@
+const mongoose = require('mongoose');
+const mongoURI = "mongodb://localhost:27017/?directConnection=true"
+
+const connectToMongo = () => {
+    mongoose.connect(mongoURI, () => {
+        console.log("Connected to mongo succesfully Amaan")
+    })
+}
+
+module.exports = connectToMongo;
